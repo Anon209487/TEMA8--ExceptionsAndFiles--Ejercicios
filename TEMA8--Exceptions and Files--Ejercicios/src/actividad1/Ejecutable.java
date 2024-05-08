5eb2c8882d3c13faadecbddd18f62d90536915e0
Anon209487 <156091669+Anon209487@users.noreply.github.com>
2024-05-08 19:55:40 +0200

    FINAL

--- TEMA8--Exceptions and Files--Ejercicios/src/actividad1/Ejecutable.java
+++ TEMA8--Exceptions and Files--Ejercicios/src/actividad1/Ejecutable.java
@@ -3,6 +3,8 @@
  */
 package actividad1;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,7 +14,27 @@ public class Ejecutable {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		doubleError(scanner);
+		intError(scanner);
+
 	}
 
+	/**
+	 * @param scanner
+	 */
+	private static void intError(Scanner scanner) {
+		System.out.println("Introduce un int");
+		int numeroInt = UtilsJava.readInt(scanner);
+		System.out.println("El valor int introducido es: " + numeroInt);
+	}
+
+	/**
+	 * @param scanner
+	 */
+	private static void doubleError(Scanner scanner) {
+		System.out.println("Introduce un double");
+		double numeroDouble = UtilsJava.readDouble(scanner);
+		System.out.println("El valor double introducido es: " + numeroDouble);
+	}
 }
